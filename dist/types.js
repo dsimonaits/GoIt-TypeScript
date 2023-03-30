6add97c512385264356853672364901d4e35b24d
Deniss Simonaits <dsimonaits@gmail.com>
2023-03-30 23:42:24 +0300

    feat: add typescript support to the project feat: add button class to the Add button in index.html refactor: add types to variables and functions in app.ts and types.ts docs: add comments to code in app.ts and types.ts
    feat(tsconfig.json): add compiler options for strict type-checking and source map generation. Add exclude for node_modules and dist folders.

---
+++ dist/types.js
@@ -0,0 +1,66 @@
+"use strict";
+let age;
+age = 50;
+let userName;
+userName = "Max";
+let toggle;
+toggle = true;
+let empty;
+empty = null;
+let notInitialize;
+notInitialize = undefined;
+let callback;
+callback = (a) => {
+    return 100 + a;
+};
+let anything;
+anything = -20;
+anything = "Text";
+anything = {};
+let some;
+some = "Text";
+let str;
+if (typeof some === "string")
+    str = some;
+let person;
+person = ["Max", 21];
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+const page = { load: Load.READY };
+if (page.load === Load.LOADING) {
+    console.log("Page loading");
+}
+if (page.load === Load.READY) {
+    console.log("Page loaded");
+}
+let union;
+let literal;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
+const page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: "2021-01-01",
+        updateAt: "2021-05-01",
+    },
+};
+const page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
+//# sourceMappingURL=types.js.map
---
+++ src/types.ts
@@ -0,0 +1,104 @@
+let age: number;
+age = 50;
+
+let userName: string;
+userName = "Max";
+
+let toggle: boolean;
+toggle = true;
+
+let empty: null;
+empty = null;
+
+let notInitialize: undefined;
+notInitialize = undefined;
+
+let callback: (a: number) => number;
+callback = (a) => {
+  return 100 + a;
+};
+
+//
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+//
+
+let some: unknown;
+
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") str = some;
+
+//
+
+let person: [string, number];
+person = ["Max", 21];
+
+//
+
+enum Load {
+  LOADING,
+  READY,
+}
+
+const page = { load: Load.READY };
+
+if (page.load === Load.LOADING) {
+  console.log("Page loading");
+}
+
+if (page.load === Load.READY) {
+  console.log("Page loaded");
+}
+
+//
+
+let union: string | number;
+
+let literal: "enable" | "disable";
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+//
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: { createAt: string; updateAt: string };
+};
+
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
